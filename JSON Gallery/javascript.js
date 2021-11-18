69b974a25a91b0114f45ab59a079eecfbb15bb88
MariaRacariu <90383391+MariaRacariu@users.noreply.github.com>
2021-11-18 11:18:33 +0100

    yes

--- JSON Gallery/javascript.js
+++ JSON Gallery/javascript.js
@@ -1,1 +1,22 @@
-function rotateImg() { document.querySelector("#img").style.transform = "scale(2)"; }
+// function scaleImg() { 
+//     document.getElementById("img").height="500"; 
+//     document.getElementById("img").width="500";
+//     document.getElementById("container").height="600px";
+//     document.getElementById("container").width="600px";
+//     document.getElementById("container").style.backgroundColor="green";
+// }
+let window = document.getElementById("pagePopUp");
+
+let img = document.getElementById("img");
+let contentImg = document.getElementById("imgWindow");
+let informationText = document.getElementById("information");
+img.onclick = function(){
+    window.style.display = "block";
+    contentImg.src = this.src;
+    informationText.innerHTML = this.alt;
+}
+let span = document.getElementsByClassName("closeBtn")[0];
+span.onclick = function(){
+    window.style.display="none";
+}
+// https://www.w3schools.com/howto/howto_css_modal_images.asp
