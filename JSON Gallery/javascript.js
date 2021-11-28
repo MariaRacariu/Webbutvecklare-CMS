a243a8b7526eece71e1ad13ce23e60a9f33fc024
MariaRacariu <90383391+MariaRacariu@users.noreply.github.com>
2021-11-28 23:31:29 +0100

    ji

--- JSON Gallery/javascript.js
+++ JSON Gallery/javascript.js
@@ -1,1 +1,12 @@
-let galleryContainer = document.querySelector(".gallery");
+let images;
+
+fetch ("paintings.json")
+.then(function (response){ 
+    return response.json();
+})
+.then(data => {
+    data.forEach(images => {
+        let titleTag = "<h1>${image.title}</h1>";
+        document.body.innerHTML += titleTag ;
+    });
+})
