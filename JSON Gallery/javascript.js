c4134b9d205ae6d2e3ee6d06df95a577a86ac77d
MariaRacariu <90383391+MariaRacariu@users.noreply.github.com>
2021-11-28 17:49:39 +0100

    Update javascript.js

--- JSON Gallery/javascript.js
+++ JSON Gallery/javascript.js
@@ -1,16 +1,29 @@
-let popUpWindow = document.getElementById("pagePopUp");
+// let popUpWindow = document.getElementById("pagePopUp");
 
-let coverImage = document.getElementById("imageBlock");
-let contentImg = document.getElementById("imgWindow");
-let informationText = document.getElementById("information");
+// let coverImage = document.getElementById("imageBlock");
+// let contentImg = document.getElementById("imgWindow");
+// let informationText = document.getElementById("information");
 
-coverImage.addEventListener("click", function display(){
-    popUpWindow.style.display = "block";
-    contentImg.src = this.src;
-    informationText.innerHTML = this.alt;
-})
-let span = document.getElementsByClassName("closeBtn")[0];
+// coverImage.addEventListener("click", function display(){
+//     popUpWindow.style.display = "block";
+//     contentImg.src = this.src;
+//     informationText.innerHTML = this.alt;
+// })
+// let span = document.getElementsByClassName("closeBtn")[0];
 
-span.onclick = function closeBtn(){
-    popUpWindow.style.display="none";
-}
+// span.onclick = function closeBtn(){
+//     popUpWindow.style.display="none";
+// }
+
+fetch("./products.json")
+    .then(function (respons) {
+        console.log(respons); //ska få status 200
+        if (respons.ok) {
+            return respons.json(); //parsea json objektet.
+        }
+    })
+    .then((data) => {
+        createProducts(data);
+        fillArray(data);
+    })
+    .catch((error) => console.log(error));
