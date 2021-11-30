cedc1c9eccb69b19e18268534d1d1d094f358a5f
MariaRacariu <90383391+MariaRacariu@users.noreply.github.com>
2021-11-30 22:22:17 +0100

    yes

--- JSON Gallery/javascript.js
+++ JSON Gallery/javascript.js
@@ -1,19 +1,25 @@
-// let imageContainer = document.querySelector(".imageBox");
-let imageList ;
+let imageContainer = document.querySelector(".imageBox");
+
+let imageList;
 //imageList is an array
 
-fetch ("paintings.json")
-.then(function (response){ 
-    return response.json();
-})
-.then(data => {
-    data.forEach(image => {
-        let titleTag = `<h1>${image.title}</h1>`;
-        let imageTag = `<img src="${image.url}" class="paintings">`;
-        document.body.innerHTML += titleTag ;
-        document.body.innerHTML += imageTag;
-    });
-    //the data from fetch is now stored in imagelist. so don't have to fetch but just call imagelist array
-    imageList = data;
-    console.log(imageList[0]);
-})
+fetch("paintings.json")
+    .then(function (response) {
+        return response.json();
+    })
+    .then(data => {
+        data.forEach(image => {
+            let titleElement = document.createElement("h1");
+            titleElement.innerHTML = image.title;
+            imageContainer.appendChild(titleElement);
+
+            let imageElement = document.createElement("img");
+            imageElement.setAttribute("class", "paintings");
+            imageElement.setAttribute("src", image.url);
+            imageContainer.appendChild(imageElement);
+        });
+        //the data from fetch is now stored in imagelist. so don't have to fetch but just call imagelist array
+        imageList = data;
+        // console.log(imageList[0]);
+    })
+
