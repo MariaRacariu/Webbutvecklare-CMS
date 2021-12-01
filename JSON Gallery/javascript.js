0263df29c25a23a3645fd2c063b88c3209e1e573
MariaRacariu <90383391+MariaRacariu@users.noreply.github.com>
2021-12-01 23:49:51 +0100

    done

--- JSON Gallery/javascript.js
+++ JSON Gallery/javascript.js
@@ -1,7 +1,19 @@
 let imageContainer = document.querySelector(".imageBox");
+let popUpBox = document.querySelector(".popUp");
+let closeBtn = document.querySelector(".closeBtn");
+let test;
 
 let imageList;
 //imageList is an array
+
+function toggle(ELEMENT, DISPLAY) {
+    if (ELEMENT.style.display === "none") {
+        ELEMENT.style.display = DISPLAY;
+    } else {
+        ELEMENT.style.display = "none";
+    }
+}
+
 
 fetch("paintings.json")
     .then(function (response) {
@@ -16,10 +28,39 @@ fetch("paintings.json")
             let imageElement = document.createElement("img");
             imageElement.setAttribute("class", "paintings");
             imageElement.setAttribute("src", image.url);
+            imageElement.setAttribute("id", image.id);
             imageContainer.appendChild(imageElement);
         });
         //the data from fetch is now stored in imagelist. so don't have to fetch but just call imagelist array
         imageList = data;
         // console.log(imageList[0]);
+        for (let i = 0; i < imageList.length; i++) {
+            document.getElementById(imageList[i].id).addEventListener("click", function () {
+                popupContainer(i);
+            });
+        }
     })
 
+function popupContainer(ITERATION) {
+    let popUp = `
+    
+        <div class="imageContainer">
+            <img src="${imageList[ITERATION].url}">
+        </div>
+        <div class="infoContainer">
+            <h1>${imageList[ITERATION].title}</h1>
+            <p>${imageList[ITERATION].artistName}</p>
+            <p>${imageList[ITERATION].year}</p>
+            <p>${imageList[ITERATION].canvasSize}</p>
+            <p>${imageList[ITERATION].description}</p>
+            <p>${imageList[ITERATION].alt}</p>
+        </div>
+    `
+    popUpBox.innerHTML = popUp;
+    toggle(popUpBox, "flex");
+    toggle(closeBtn, "block");
+}
+closeBtn.addEventListener("click", function () {
+    toggle(popUpBox, "flex");
+    toggle(this, "block");
+});
